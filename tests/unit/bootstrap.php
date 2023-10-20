98a4c0d1d05764f1f4f165b813cb053188d7fa97
Aaron Weeden <aaronwee@buffalo.edu>
2023-10-20 12:48:41 -0400

    Refactor integration test namespaces.

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -5,9 +5,47 @@ $dir = __DIR__;
 // Autoloader for main framework test classes.
 spl_autoload_register(
     function ($className) use ($dir) {
-        $classPath
-            = $dir
-            . '/../../../xdmod/open_xdmod/modules/xdmod/integration_tests/lib/'
+        // Replace the UnitTests namespace prefix with the path to the unit
+        // tests lib directory.
+        $classPath = preg_replace(
+            '/UnitTests\\\\?/',
+            "$dir/lib/",
+            $className
+        );
+        // Replace the IntegrationTests namespace prefix with the path to the
+        // integration tests lib directory.
+        $classPath = preg_replace(
+            '/IntegrationTests\\\\?/',
+            "$dir/../integration/lib/",
+            $classPath
+        );
+        // Replace namespace separators with directory separators.
+        $classPath = str_replace('\\', '/', $classPath) . '.php';
+        if (is_readable($classPath)) {
+            return require_once $classPath;
+        }
+        // Replace the UnitTests namespace prefix with the path to the main
+        // unit tests lib directory.
+        $classPath = preg_replace(
+            '/UnitTests\\\\?/',
+            "$dir/../../../xdmod/tests/unit/lib/",
+            $className
+        );
+        // Replace the IntegrationTests namespace prefix with the path to
+        // the main integration tests lib directory.
+        $classPath = preg_replace(
+            '/IntegrationTests\\\\?/',
+            "$dir/../../../xdmod/tests/integration/lib/",
+            $classPath
+        );
+        // Replace namespace separators with directory separators.
+        $classPath = str_replace('\\', '/', $classPath) . '.php';
+        if (is_readable($classPath)) {
+            return require_once $classPath;
+        }
+        // Autoload the AppKernels module classes
+        $classPath = (
+            "$dir/../../classes/"
             . str_replace('\\', '/', $className)
             . '.php';
 
